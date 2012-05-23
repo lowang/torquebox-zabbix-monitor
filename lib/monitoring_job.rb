fc6761ca176c066075908cde249d25b5334328d1
Przemek Wroblewski <przemyslaw.wroblewski@gmail.com>
2012-05-23 11:20:29 +0200

    duplicate messages_added key using different name due to zabbix unique key check. This will allow me to have two items, one defined as raw and second defined as delta.

--- lib/monitoring_job.rb
+++ lib/monitoring_job.rb
@@ -23,6 +23,7 @@ class MonitoringJob
       tb_stats.get_queue_stats.each do |queue,stats|
         stats.each do |key,value|
           zabbix.send(hostname, queue.to_s + '.' + key.to_s, value.to_s, @options['debug'])
+          zabbix.send(hostname, queue.to_s + '.' + key.to_s + '_rate', value.to_s, @options['debug']) if key.to_s == 'messages_added'
         end
       end
     end
